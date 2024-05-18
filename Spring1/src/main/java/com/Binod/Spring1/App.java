912ef071b889db2dd4e1f4fef8187fed039dd901
Binod <bidhaksubedi77@gmail.com>
2024-05-18 11:10:21 +0530

    setter injections

--- Spring1/src/main/java/com/Binod/Spring1/App.java
+++ Spring1/src/main/java/com/Binod/Spring1/App.java
@@ -11,13 +11,27 @@ public class App
     	
     	ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml"); //makesure to have spring bean xml config
     	Programmer obj= (Programmer) context.getBean("programmer");
-    	
-    	//mathi ko step le Container banaux 
-    	//steps::
-    		//pom.xml ma Spring context dependecny halne
-    		//ewta xml file banaune
-    		//spring xml beans config google bata liyerw paste garne
+//    	
+//    	//mathi ko step le Container banaux 
+//    	//steps::
+//    		//pom.xml ma Spring context dependecny halne
+//    		//ewta xml file banaune
+//    		//spring xml beans config google bata liyerw paste garne
     	obj.doCode();
+//    	
+    	
+    	// setter injection
+    	// we use <property> tag in out xml file
+//    	flow::
+//    		1. create the object
+//    		2.call the setter method
+//    		3.Execute other parts
+    	//make sure that the class is not declared private.
+    	
+    	coder obj1 = (coder) context.getBean("Coder");  
+    	System.out.println("the age defined through the setter injectio is "+ obj1.getAge());
+    	
+    	
     }
 }
 
---
+++ Spring1/src/main/java/com/Binod/Spring1/coder.java
@@ -0,0 +1,14 @@
+package com.Binod.Spring1;
+
+public class coder {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("The setter method is being called");
+        this.age = age;
+    }
+}
